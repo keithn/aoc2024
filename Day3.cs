db1ec0d5b65a610dacb8ed863b0552d08b5b6e65
Keith Nicholas <keith.nicholas@gmail.com>
2024-12-14 22:57:23 +1300

    Day3

---
+++ Day3.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Aoc2024;
+
+public class Day3
+{
+    private class Context( bool AlwaysEnabled )
+    {
+        public bool Enabled { get => field | AlwaysEnabled; set; } = true;
+    }
+    public static void Solve(string input)
+    {
+        var operations = Regex.Matches(input, @"(mul|do|don't)\((\d+)?,?(\d+)?\)")
+            .Select(m => (Op: m.Groups[1].Value, A: m.Groups[2].Value, B: m.Groups[3].Value))
+            .ToList();
+
+        int Evaluate(string op, string a, string b, Context context) =>
+            op switch
+            {
+                "do" => (context.Enabled = true, 0).Item2,
+                "don't" => (context.Enabled = false, 0).Item2,
+                "mul" => (context.Enabled) ? int.Parse(a) * int.Parse(b) : 0,
+                _ => 0
+            };
+
+        int Calculate(Context context) => operations.Sum(op => Evaluate(op.Op, op.A, op.B, context));
+
+        Console.WriteLine(Calculate(new Context(true))); // Part 1
+        Console.WriteLine(Calculate(new Context(false))); // Part 2
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,7 +3,8 @@
 using Aoc2024;
 
 // Day1.Solve(Load("Day1.txt"));
-Day2.Solve(Load("Day2.txt"));
+// Day2.Solve(Load("Day2.txt"));
+Day3.Solve(Load("Day3.txt"));
 
 
 string Load(string file) => File.ReadAllText(Path.ChangeExtension(file, ".txt"));
