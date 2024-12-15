e328456d2ce8e15e8c3a1db0c4def9c61a061551
Keith Nicholas <keith.nicholas@gmail.com>
2024-12-15 19:08:57 +1300

    day5

--- Day5.cs
+++ Day5.cs
@@ -2,5 +2,29 @@
 
 public class Day5
 {
-    
+    public static void Solve(string input)
+    {
+        var lines = input.Split('\n', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+        var rules = lines.Where(l => l.Contains('|')).Select(l => l.Split('|').Select(int.Parse).ToArray()).ToArray();
+        var updates = lines.Where(l => !l.Contains('|')).Select(l => l.Split(',').Select(int.Parse).ToArray()).ToList();
+
+        bool FollowsRules(int[] update)
+        {
+            var notAllowed = new HashSet<int>();
+            return update.All(page =>
+            {
+                if (notAllowed.Contains(page)) return false;
+                notAllowed.UnionWith(rules.Where(r => r[1] == page).Select(r => r[0]));
+                return true;
+            });
+        }
+
+        var part1 = updates.Where(FollowsRules).Select(u => u[u.Length / 2]).Sum();
+        Console.WriteLine(part1);
+
+        int RuleComparer(int x, int y) => rules.Any(r => r[0] == x && r[1] == y) ? -1 : 1;
+        var part2 = updates.Where(u => !FollowsRules(u)).Select(u => u.Order(Comparer<int>.Create(RuleComparer)).ToArray())
+            .Select(u => u[u.Length / 2]).Sum();
+        Console.WriteLine(part2);
+    }
 }
--- Program.cs
+++ Program.cs
@@ -5,7 +5,9 @@ using Aoc2024;
 // Day1.Solve(Load("Day1.txt"));
 // Day2.Solve(Load("Day2.txt"));
 // Day3.Solve(Load("Day3.txt"));
-Day4.Solve(Load("Day4.txt"));
+// Day4.Solve(Load("Day4.txt"));
+Day5.Solve(Load("Day5Example.txt"));
+Day5.Solve(Load("Day5.txt"));
 
 
 string Load(string file) => File.ReadAllText(Path.ChangeExtension(file, ".txt"));
