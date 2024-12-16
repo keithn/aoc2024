707b1a497c2f96152ef6c5fd49c843128c700307
Keith Nicholas <keith.nicholas@gmail.com>
2024-12-16 17:05:21 +1300

    Day7

---
+++ Day7.cs
@@ -0,0 +1,36 @@
+﻿namespace Aoc2024;
+
+public class Day7
+{
+    public record Calibration(long Value, long[] Weights)
+    {
+        public static Calibration Parse(string s)
+        {
+            var parts = s.Split(':');
+            var value = long.Parse(parts[0]);
+            var weights = parts[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToArray();
+            return new Calibration(value, weights);
+        }
+        public bool IsValid(bool allowConcat) => CheckCombination(0, Weights[0], allowConcat);
+
+        private bool CheckCombination(int index, long current, bool allowConcat)
+        {
+            if (index == Weights.Length - 1) return current == Value;
+            var next = Weights[index + 1];
+            return CheckCombination(index + 1, current + next, allowConcat) ||
+                   CheckCombination(index + 1, current * next, allowConcat) ||
+                   (allowConcat && CheckCombination(index + 1, Concat(current, next), allowConcat));
+        }
+        long Concat(long a, long b) => long.Parse($"{a}{b}");
+    }
+
+    public static void Solve(string input)
+    {
+        var lines = input.Split('\n', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+        var calibrations = lines.Select(Calibration.Parse).ToList();
+        var part1 = calibrations.Where(c => c.IsValid(false)).Sum(c => c.Value);
+        Console.WriteLine(part1);
+        var part2 = calibrations.Where(c => c.IsValid(true)).Sum(c => c.Value);
+        Console.WriteLine(part2);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Diagnostics;
 using Aoc2024;
 
 // Day1.Solve(Load("Day1.txt"));
@@ -8,7 +9,9 @@ using Aoc2024;
 // Day4.Solve(Load("Day4.txt"));
 // Day5.Solve(Load("Day5Example.txt"));
 // Day5.Solve(Load("Day5.txt"));
-Day6.Solve(Load("Day6Example.txt"));
-Day6.Solve(Load("Day6.txt"));
+// Day6.Solve(Load("Day6Example.txt"));
+// Day6.Solve(Load("Day6.txt"));
+Day7.Solve(Load("Day7Example.txt"));
+Day7.Solve(Load("Day7.txt"));
 
 string Load(string file) => File.ReadAllText(Path.ChangeExtension(file, ".txt"));
